ec8386b7742b3bcbb49c81d4c18ea24a76e4aed7
unknown <kalilulekan@gmail.com>
2022-09-15 12:21:42 +0100

    adding router to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './components/pages/Homepage.component';
 
+function Title () {
+  return(
+    <div>
+       <h1>welcome to men pages</h1>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-    <HomePage />
+      <Route exact path='/shop/hats' component={Title} />
+      <Route exact path='/' component={HomePage} />
+
     </div>
   );
 }
