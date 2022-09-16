09974d6ae7886796bd50da2612921cdade4d2394
unknown <kalilulekan@gmail.com>
2022-09-16 14:15:39 +0100

    adding collection preview

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import { Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './components/pages/Homepage.component';
+import HomePage from './components/pages/homepage/Homepage.component';
+import ShopPage from './components/pages/shop-page/ShopPage.component';
 
-function Title () {
-  return(
-    <div>
-       <h1>welcome to men pages</h1>
-    </div>
-  )
-}
+
 
 function App() {
   return (
     <div className="App">
-      <Route exact path='/shop/hats' component={Title} />
+      <Route exact path='/shop' component={ShopPage} />
       <Route exact path='/' component={HomePage} />
 
     </div>
---
+++ src/components/collection-item/collectionItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './collection-item.styles.scss'
+
+const CollectionItem = () =>{
+    return(
+        <div>
+            <div></div>
+        </div>
+    )
+}
+export default CollectionItem;
