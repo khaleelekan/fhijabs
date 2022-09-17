d026f9c76678f6e859ad8c7b11e62dc27425830b
unknown <kalilulekan@gmail.com>
2022-09-17 21:34:57 +0100

    adding header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom';
 
 import './App.css';
+import Header from './components/header/header-component';
 
 import HomePage from './components/pages/homepage/Homepage.component';
 import ShopPage from './components/pages/shop-page/ShopPage.component';
@@ -11,6 +12,7 @@ import ShopPage from './components/pages/shop-page/ShopPage.component';
 function App() {
   return (
     <div className="App">
+      <Header />
       <Route exact path='/shop' component={ShopPage} />
       <Route exact path='/' component={HomePage} />
 
--- src/components/collection-item/collectionItem.jsx
+++ src/components/collection-item/collectionItem.jsx
@@ -7,10 +7,10 @@ const CollectionItem = ({id, name, price, imageUrl}) =>{
             <div
                className="image"
                style={{backgroundImage: `url(${imageUrl})`}}>
+                </div>
                 <div className="collection-footer">
                     <span className="name">{name}</span>
                     <span className="price">{price}</span>
-                </div>
             </div>
         </div>
     )
