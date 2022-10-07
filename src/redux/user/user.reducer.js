5bd34828dcb18f644364445b0407353cbb6977a6
unknown <kalilulekan@gmail.com>
2022-10-07 21:50:45 +0100

    reducer function

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,9 +1,18 @@
 
-const INITIAL_STATE_STATE = {
-    currentUser : null;
+const INITIAL_STATE = {
+    currentUser : null
 }
 
 
 const userReducer = (state= INITIAL_STATE, action) => {
-    switch
+    switch(action.type){
+        case'SET_CURRENT_USER':
+        return{
+            ...state,
+            currentUser: action.payload
+        }
+
+        default:
+            return state;
+    }
 }
